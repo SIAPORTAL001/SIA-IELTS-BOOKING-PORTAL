27c1bfcbf25e4804f01e92f2710fb464b59daa1d
SIAPORTAL001 <siaportal0@gmail.com>
2026-01-02 13:25:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 alert("JavaScript working");
 let id = "SIA-" + Math.floor(Math.random()*10000);
 alert(id);
+import { db } from "./firebase.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+document.querySelector("form").addEventListener("submit", async function(e){
+  e.preventDefault();
+
+  let data = {
+    name: document.querySelector("input[placeholder='Full Name']").value,
+    email: document.querySelector("input[placeholder='Email']").value,
+    test: document.querySelector("select").value,
+    date: document.querySelector("input[type='date']").value,
+    time: document.querySelector("input[type='time']").value,
+    candidateID: "SIA-" + Math.floor(Math.random()*100000),
+    examCode: "EX-" + Math.floor(Math.random()*100000)
+  };
+
+  await addDoc(collection(db, "bookings"), data);
+  alert("Booking saved successfully");
+});
